4e89682550cc1b08c2b43b5eaf3f3a44445dd5c0
edwardyang12 <edwardyang016@gmail.com>
2021-12-26 12:48:57 -0800

    video and frontend start

--- requester.py
+++ requester.py
@@ -5,7 +5,7 @@ import numpy as np
 resp = requests.post("http://localhost:5000/predict",
                      files={"file": open('00000.png','rb')})
 
-img = resp.json()['temp']
+img = resp.json()['temp'] # simple test to make sure flask was able to send and receive a simple image 
 img = np.array(img)
 img = ((img[0]*0.5)+0.5)*255.
 img = img.transpose(1,2,0)
