3b88a66d61b42875b889ff0bb9c7e2a0bef40aeb
root <edwardyang016@gmail.com>
2021-12-26 20:59:53 +0000

    updated lists

--- list.py
+++ list.py
@@ -2,8 +2,8 @@ import pandas as pd
 from os import listdir
 from os.path import isfile, join
 
-path = "/edward-slow-vol/test"
+path = "/edward-slow-vol/Faces/"
 
 files = [join(path,f) for f in listdir(path) if isfile(join(path,f))]
 df = pd.DataFrame(files)
-df.to_csv('list_test_faces.csv')
+df.to_csv('list_faces.csv')
