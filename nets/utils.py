dce9cfccc2e5eaecc9e2c37a06f18229946ca183
edwardyang12 <edwardyang016@gmail.com>
2021-12-27 22:07:36 -0800

    should be fixed

--- nets/utils.py
+++ nets/utils.py
@@ -44,30 +44,30 @@ def weights_init(m):
 # https://github.com/sanghviyashiitb/GANS-VanillaAndMinibatchDiscrimination/blob/master/minibatch_discrimination.py
 # reduce mode collapse by appending similarity value to discriminated outputs
 class MiniBatchDiscrimination(nn.Module):
-	def __init__(self, A, B, C, batch_size):
-		super(MiniBatchDiscrimination, self).__init__()
-		self.feat_num = A
-		self.out_size = B
-		self.row_size = C
-		self.N = batch_size
-		self.T = Parameter(torch.Tensor(A,B,C)).to("cuda")
-		self.reset_parameters()
+    def __init__(self, A, B, C, batch_size):
+        super(MiniBatchDiscrimination, self).__init__()
+        self.feat_num = A
+        self.out_size = B
+        self.row_size = C
+        self.N = batch_size
+        self.T = Parameter(torch.Tensor(A,B,C))
+        self.reset_parameters()
 
-	def forward(self, x):
-		# Output matrices after matrix multiplication
-		M = x.mm(self.T.view(self.feat_num,self.out_size*self.row_size)).view(-1,self.out_size,self.row_size)
-		out = Variable(torch.zeros(self.N,self.out_size), requires_grad=True).to("cuda")
-		for k in range(self.N): # Not happy about this 'for' loop, but this is the best we could do using PyTorch IMO
-			c = torch.exp(-torch.sum(torch.abs(M[k,:]-M),2)) # exp(-L1 Norm of Rows difference)
-			if k != 0 and k != self.N -1:
-				out[k,:] = torch.sum(c[0:k,:],0) + torch.sum(c[k:-1,:],0)
-			else:
-				if k == 0:
-					out[k,:] = torch.sum(c[1:,:],0)
-				else:
-					out[k,:] = torch.sum(c[0:self.N-1],0)
-		return out
+    def forward(self, x):
+        device = self.T.device
+        M = x.mm(self.T.view(self.feat_num,self.out_size*self.row_size)).view(-1,self.out_size,self.row_size)
+        out = Variable(torch.zeros(self.N,self.out_size)).to(device)
+        for k in range(self.N): # Not happy about this 'for' loop, but this is the best we could do using PyTorch IMO
+            c = torch.exp(-torch.sum(torch.abs(M[k,:]-M),2)) # exp(-L1 Norm of Rows difference)
+            if k != 0 and k != self.N -1:
+                out[k,:] = torch.sum(c[0:k,:],0) + torch.sum(c[k:-1,:],0)
+            else:
+                if k == 0:
+                    out[k,:] = torch.sum(c[1:,:],0)
+                else:
+                    out[k,:] = torch.sum(c[0:self.N-1],0)
+        return out
 
-	def reset_parameters(self):
-		stddev = 1/self.feat_num
-		self.T.data.uniform_(stddev)
+    def reset_parameters(self):
+        stddev = 1/self.feat_num
+        self.T.data.uniform_(stddev)
